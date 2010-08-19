f40236f2ebb55b5d264091e16ef301bfc7612cc5
Justin Force <justin.force@gmail.com>
2010-08-19 11:05:27 -0700

    Retab/white space

--- dist/youtube_popout.js
+++ dist/youtube_popout.js
@@ -5,74 +5,74 @@
 
 jQuery.noConflict();
 (function($) {
-	$(function() {
+  $(function() {
 
-		// The movie player object
-		var player = $('#movie_player');
+    // The movie player object
+    var player = $('#movie_player');
 
-		var buttonHTML = '<button id=YouTubePopoutButton>';
-		buttonHTML += '<img width=29 height=29';
-		buttonHTML += ' alt="'+chrome.i18n.getMessage('buttonText')+'"';
-		buttonHTML += ' title="'+chrome.i18n.getMessage('buttonText')+'">';
-		buttonHTML += '</button>'; 
+    var buttonHTML = '<button id=YouTubePopoutButton>';
+    buttonHTML += '<img width=29 height=29';
+    buttonHTML += ' alt="'+chrome.i18n.getMessage('buttonText')+'"';
+    buttonHTML += ' title="'+chrome.i18n.getMessage('buttonText')+'">';
+    buttonHTML += '</button>'; 
 
-		var button = $(buttonHTML).css({ position: 'absolute' });
+    var button = $(buttonHTML).css({ position: 'absolute' });
 
-		$('body').append(button);
+    $('body').append(button);
 
-		disableButton();
+    disableButton();
 
-		// Reposition the button when the window is resized
-		$(window).resize(function(e) {
-			var offset = player.offset();
-			button.css({
-				top: offset.top - button.height(),
-				left: offset.left + player.width() - button.width(),
-				zIndex: 99999
-			});
-		});
-		$(window).resize(); // Position the button immediately
+    // Reposition the button when the window is resized
+    $(window).resize(function(e) {
+      var offset = player.offset();
+      button.css({
+        top: offset.top - button.height(),
+        left: offset.left + player.width() - button.width(),
+        zIndex: 99999
+      });
+    });
+    $(window).resize(); // Position the button immediately
 
-		button.click(function() {
+    button.click(function() {
 
-			// Assign player and re-enable button in case video has changed
-			player = $('#movie_player'); 
-			disableButton();
-			tryToEnableButton();
+      // Assign player and re-enable button in case video has changed
+      player = $('#movie_player'); 
+      disableButton();
+      tryToEnableButton();
 
-			player.get(0).pauseVideo();
+      player.get(0).pauseVideo();
 
       // Tell background.html to open new window. Grab a fresh copy of the HTML
       // for the embed tag and its friends.
-			chrome.extension.sendRequest({set: true, html: $('#watch-video').html(), title: document.title});
+      chrome.extension.sendRequest({set: true, html: $('#watch-video').html(), title: document.title});
 
-			return false;
-		});
+      return false;
+    });
 
-		function tryToEnableButton() {
+    function tryToEnableButton() {
 
-			// timeout to recheck whether player is ready
-			if (this.timeout)
-				clearTimeout(this.timeout);
-			else
-				this.timeout = null;
+      // timeout to recheck whether player is ready
+      if (this.timeout)
+        clearTimeout(this.timeout);
+      else
+        this.timeout = null;
 
-			// Don't enable button until the player is fully initialized
-			if (!player.get(0).getPlayerState)
-				this.timeout = setTimeout(tryToEnableButton,500);
-			else {
-				button.removeAttr('disabled').css( {cursor: 'pointer' }).children('img').hover(function() {
-					$(this).attr('src', chrome.extension.getURL('icon29.png'));
-				}, function() {
-					$(this).attr('src', chrome.extension.getURL('icon29disabled.png'));
-				});
-			}
-		}
-		tryToEnableButton();
+      // Don't enable button until the player is fully initialized
+      if (!player.get(0).getPlayerState)
+        this.timeout = setTimeout(tryToEnableButton,500);
+      else {
+        button.removeAttr('disabled').css( {cursor: 'pointer' }).children('img').hover(function() {
+          $(this).attr('src', chrome.extension.getURL('icon29.png'));
+        }, function() {
+          $(this).attr('src', chrome.extension.getURL('icon29disabled.png'));
+        });
+      }
+    }
+    tryToEnableButton();
 
-		function disableButton() {
-			button.attr('disabled', 'disabled').css({cursor: 'default'}).children('img').unbind('mouseenter mouseleave').attr('src', chrome.extension.getURL('icon29disabled.png'));
-		}
-	});
+    function disableButton() {
+      button.attr('disabled', 'disabled').css({cursor: 'default'}).children('img').unbind('mouseenter mouseleave').attr('src', chrome.extension.getURL('icon29disabled.png'));
+    }
+  });
 })(jQuery);
 
