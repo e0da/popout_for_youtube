88275bb4cd155f4fc21aa696960613ff6716bd49
Justin Force <justin.force@gmail.com>
2011-05-24 17:02:20 -0700

    Start building HTML5/Flash version.

--- dist/popout_for_youtube.js
+++ dist/popout_for_youtube.js
@@ -4,75 +4,39 @@
 // License: GPL http://www.gnu.org/licenses/gpl.html
 
 jQuery.noConflict();
-(function($) {
-  $(function() {
+(function($) { $(function() {
 
-    // The movie player object
-    var player = $('#movie_player');
+  var selector = $('video') ? 'video' : '#movie_player';
 
-    var buttonHTML = '<button id=YouTubePopoutButton>';
-    buttonHTML += '<img width=29 height=29';
-    buttonHTML += ' alt="'+chrome.i18n.getMessage('buttonText')+'"';
-    buttonHTML += ' title="'+chrome.i18n.getMessage('buttonText')+'">';
-    buttonHTML += '</button>'; 
-
-    var button = $(buttonHTML).css({ position: 'absolute' });
-
-    $('body').append(button);
-
-    disableButton();
-
-    // Reposition the button when the window is resized
-    $(window).resize(function(e) {
-      var offset = player.offset();
-      button.css({
-        top: offset.top - button.height(),
-        left: offset.left + player.width() - button.width(),
-        zIndex: 99999
-      });
-    });
-    $(window).resize(); // Position the button immediately
-
-    button.click(function() {
-
-      // Assign player and re-enable button in case video has changed
-      player = $('#movie_player'); 
-      disableButton();
-      tryToEnableButton();
-
-      player.get(0).pauseVideo();
-
-      // Tell background.html to open new window. Grab a fresh copy of the HTML
-      // for the embed tag and its friends.
-      chrome.extension.sendRequest({set: true, html: $('#watch-video').html(), title: document.title});
-
-      return false;
-    });
-
-    function tryToEnableButton() {
-
-      // timeout to recheck whether player is ready
-      if (this.timeout)
-        clearTimeout(this.timeout);
-      else
-        this.timeout = null;
-
-      // Don't enable button until the player is fully initialized
-      if (!player.get(0).getPlayerState)
-        this.timeout = setTimeout(tryToEnableButton,500);
-      else {
-        button.removeAttr('disabled').css( {cursor: 'pointer' }).children('img').hover(function() {
-          $(this).attr('src', chrome.extension.getURL('icon29.png'));
-        }, function() {
-          $(this).attr('src', chrome.extension.getURL('icon29disabled.png'));
-        });
-      }
-    }
-    tryToEnableButton();
-
-    function disableButton() {
-      button.attr('disabled', 'disabled').css({cursor: 'default'}).children('img').unbind('mouseenter mouseleave').attr('src', chrome.extension.getURL('icon29disabled.png'));
-    }
+  $(window).bind('playerReady', function(e) {
+    var player = getPlayer();
+    console.log(player.getPlayerState());
   });
-})(jQuery);
+
+  function getPlayer() {
+    return $('#movie_player').get(0);
+  }
+
+  var checkPlayerReady = setInterval(function() {
+
+    function trigger() {
+      clearInterval(checkPlayerReady);
+      $(window).trigger('playerReady');
+    }
+
+    try {
+      $('video').get(0).readyState && trigger();
+    } catch (e) {
+      console.log(e);
+    }
+
+    try {
+      $('#movie_player').get(0).getPlayerState && trigger();
+    } catch(e) {
+      console.log(e);
+    }
+    console.log('checked');
+  }, 200);
+
+}); })(jQuery);
 
