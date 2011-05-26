5ca6f060149440899e365ec53770a23d59359e8a
Justin Force <justin.force@gmail.com>
2011-05-26 11:22:04 -0700

    Get all major functionality working.

--- dist/popout_for_youtube.js
+++ dist/popout_for_youtube.js
@@ -11,12 +11,32 @@ jQuery.noConflict();
   var FLASH = 0x1;
   var html5Selector = 'video';
   var flashSelector = '#movie_player';
+  var iconUrl = chrome.extension.getURL('icon29.png');
+  var text = chrome.i18n.getMessage('buttonText');
+
+  var css = {
+    textIndent: '-999em',
+    width: 29,
+    height: 29,
+    background: 'url(' + iconUrl + ')',
+    position: 'absolute',
+    right: 0,
+    top: -30
+  };
 
   /* The YouTube player */
   var player = null;
 
+  /* The YouTube video ID */
+  var id = null;
+
   /* The player type (HTML5 or Flash) */
   var type = null;
+
+  /* The parent of the button (where we append it). Varies between players. By
+  * choosing a good parent, we can get the button to automatically align to the
+  * right edge of the player. */
+  var buttonParent = null;
 
   /* Get the player */
   getPlayer();
@@ -29,7 +49,7 @@ jQuery.noConflict();
   /* Wait until the player is loaded and ready (the custom playerReady event is
   * triggered) to get started */
   $(window).bind('playerReady', function(e) {
-    normalizePlayer();
+    normalize();
     addButton();
   });
 
@@ -45,6 +65,7 @@ jQuery.noConflict();
     /* Default to HTML5 player, then fall back to the Flash player */
     player = $('video');
     type = HTML5;
+    id = document.URL.match(/\?.*v=([^\&]+)/)[1]; /* pull ID from URL */
 
     if (player.length === 0) {
       player = $('#movie_player');
@@ -65,10 +86,13 @@ jQuery.noConflict();
     }
   }
 
-  /* Set up normalized API on top of player of either type */
-  function normalizePlayer() {
+  /* Normalize the environment for all player types.*/
+  function normalize() {
     var node = player.get(0);
     if (type === HTML5) {
+
+      buttonParent = player.parent().parent().parent();
+
       player.state = function() {
         return node.readyState;
       }
@@ -86,6 +110,9 @@ jQuery.noConflict();
       }
     }
     else { /* type === FLASH */
+
+      buttonParent = player.parent();
+
       player.pause = function() {
         return node.pauseVideo();
       }
@@ -105,34 +132,30 @@ jQuery.noConflict();
   }
 
   function addButton() {
-    /*
-    * TODO
-    *
-    * Create and add the button. Should be inside the container that holds the
-    * video so that it is aligned to the right and directly above the video.
-    *
-    */
-    var button = $('<button>Popout</button>');
-    $('body').prepend(button);
+
+    /* Create the button and append it to the previously defined buttonParent. */
+    var button = $('<button>').text(text).css(css).attr('title', text);
+    buttonParent.append(button);
 
     button.click(popout);
   }
 
   function popout() {
-    /* Pause video, note current time. */
+
+    /* Pause video, note current time and dimensions */
     player.pause();
     var time = player.time();
+    var width = player.outerWidth(true);
+    var height = player.outerHeight(true);
+
 
     /* Run it back a couple seconds. If we've only been playing a few seconds,
     * start the popped out video at the beginning. */
-    time = time < 10 ? 0 : time - 2;
-    
-    /*
-    * TODO
-    *
-    * Create new window, copy player to it, advance to current time, and play
-    * video. This requires utilizing the Chrome API messaging system.
-    */
+    time = time < 10 ? 0 : time - 3;
+
+    /* Tell background.html to create new window containing video with current
+    * time and dimentions. */
+    chrome.extension.sendRequest({id: id, title: document.title, time: time, width: width, height: height});
   }
 }); })(jQuery);
 
